da46c207d612e80ebc79a84b3450b20552bf792e
unknown <no0104@CLASSROOM.NET>
2018-07-12 19:50:49 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto03_ECommerce/Controllers/ClientesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto03_ECommerce.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Incluir()
+        {
+            return View();
+        }
+        public ActionResult Listar()
+        {
+            return View();
+        }
+    }
+}
